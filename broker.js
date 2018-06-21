99139f3700dbfa46a1a1e6653fd6100a7717d7db
Hung Le <lehungmt94@gmail.com>
2018-06-21 14:37:21 +0700

    install  Project

---
+++ broker.js
@@ -0,0 +1,64 @@
+
+var mosca = require('mosca');
+var settings = {
+		port:1883
+		}
+
+var server = new mosca.Server(settings);
+
+server.on('ready', function(){
+console.log(".........PORTAL IoT Starting OK......");
+var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+         console.log(alias.address);
+    }
+  }
+
+});
+
+// fired whena  client is connected
+server.on('clientConnected', function(client) {
+  console.log('message from server == client connected', client.id);
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('message from server == Published : ', packet.payload.toString());
+});
+
+// fired when a client subscribes to a topic
+server.on('subscribed', function(topic, client) {
+  console.log('message from server == subscribed : ', topic);
+});
+
+// fired when a client subscribes to a topic
+server.on('unsubscribed', function(topic, client) {
+  console.log('message from server == unsubscribed : ', topic);
+ });
+
+// fired when a client is disconnecting
+server.on('clientDisconnecting', function(client) {
+  console.log('message from server == clientDisconnecting : ', client.id);
+});
+
+// fired when a client is disconnected
+server.on('clientDisconnected', function(client) {
+  console.log('message from server == clientDisconnected : ', client.id);
+});
+
+
+var http = require('http');
+var date = new Date();
+var http_server = http.createServer(function(req, res) {
+	date =Date();
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Le Hung MQTT Hello world!\n');
+  res.write(date.toString());
+  res.end('');
+});
+http_server.listen(80);
